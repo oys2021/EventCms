4ff1739ad620f206c83195d37246f7640f9c4f06
Yaw Sarfo <yawsarfo2019@gmail.com>
2023-12-04 08:16:13 +0000

    i updated the staticfiles configurations

--- myevents/urls.py
+++ myevents/urls.py
@@ -18,7 +18,7 @@ from django.contrib import admin
 from django.urls import path,include
 from django.conf import settings
 from django.conf.urls.static import static
-# from django.contrib.staticfiles.urls import staticfiles_urlpatterns
+from django.contrib.staticfiles.urls import staticfiles_urlpatterns
 
 
 urlpatterns = [
@@ -27,5 +27,6 @@ urlpatterns = [
     path('dashboard/', include('administrator.urls')),
 ]
 
-if settings.DEBUG:
-    urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
+urlpatterns += staticfiles_urlpatterns()
+# if settings.DEBUG:
+#     urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
